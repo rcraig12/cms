6a2cfa53ae6741014843213af79aedb1c844e8d4
rcraig12 <rcraig12@gmail.com>
2017-10-14 10:58:34 +0100

    first commit

---
+++ src/CMSServiceProvider.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace RCS\CMS;
+
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Foundation\AliasLoader;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\View;
+use RCS\CMS\Facades\CMS as CMSFacade;
+
+class CMSServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $loader = AliasLoader::getInstance();
+        $loader->alias('CMS', CMSFacade::class);
+        $this->app->singleton('CMS', function () {
+            return new CMS();
+        });
+
+        if ($this->app->runningInConsole()) {
+            /*$this->registerPublishableResources();*/
+            $this->registerConsoleCommands();
+        }
+    
+    }
+
+    /**
+     * Register the commands accessible from the Console.
+     */
+    private function registerConsoleCommands()
+    {
+        $this->commands(Commands\InstallCommand::class);
+
+    }
+
+}
---
+++ src/Traits/Seedable.php
@@ -0,0 +1,13 @@
+<?php
+namespace RCS\CMS\Traits;
+
+trait Seedable
+{
+    public function seed($class)
+    {
+        if (!class_exists($class)) {
+            require_once $this->seedersPath.$class.'.php';
+        }
+        with(new $class())->run();
+    }
+}
